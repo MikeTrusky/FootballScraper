911dfe734255155e3e7643a25a5d5e54fff67376
Maciej Trzuskowski <miketrusky270696@gmail.com>
2025-01-19 15:29:52 +0100

    Changed loading data - now loading avg values for each of the previous x matches.

--- football-data.py
+++ football-data.py
@@ -11,15 +11,8 @@ MATCH_ID = int(sys.argv[1])
 NUM_MATCHES = int(sys.argv[2])
 COMPETITION_ID = int(sys.argv[3])
 
-def get_team_info(homeMatch, teamId, teamName):
-    goalsScoredAll = 0
-    goalsScoredHomeOrAway = 0
-    goalsConcededAll = 0
-    goalsConcededHomeOrAway = 0
-    matchesHome = 0
-    matchesAway = 0
-
-    url = f"{URL_BEGIN}/teams/{teamId}/matches?status=FINISHED&competitions={COMPETITION_ID}&dateFrom={DATE_FROM}&dateTo={DATE_TO}&limit={NUM_MATCHES}"
+def get_team_results(homeMatch, teamId):
+    url = f"{URL_BEGIN}/teams/{teamId}/matches?status=FINISHED&competitions={COMPETITION_ID}&dateFrom={DATE_FROM}&dateTo={DATE_TO}&limit={NUM_MATCHES * 2}"
 
     headers = {
         "X-Auth-Token": get_api_key()
@@ -27,6 +20,24 @@ def get_team_info(homeMatch, teamId, teamName):
 
     response = requests.get(url, headers=headers)
     matches = response.json()['matches']
+
+    results = []
+    matches.reverse()
+    for index, match in enumerate(matches[:NUM_MATCHES]):
+        if index == 0:
+            results.append(get_team_match_info(homeMatch, teamId, 0, matches[index:(index + 5)], 0))
+        else:
+            results.append(get_team_match_info(homeMatch, teamId, matches[(index - 1)]["id"], matches[index:(index + 5)], matches[(index - 1)]["score"]["fullTime"]["home"] + matches[(index - 1)]["score"]["fullTime"]["away"]))
+
+    return results
+
+def get_team_match_info(homeMatch, teamId, matchId, matches, matchTotalGoals):
+    goalsScoredAll = 0
+    goalsScoredHomeOrAway = 0
+    goalsConcededAll = 0
+    goalsConcededHomeOrAway = 0
+    matchesHome = 0
+    matchesAway = 0
 
     for match in matches:
         if match["homeTeam"]["id"] == teamId:
@@ -50,7 +61,7 @@ def get_team_info(homeMatch, teamId, teamName):
     goalsConcededHomeOrAwayAvg = round(goalsConcededHomeOrAway / matchesHome, 2)
 
     return {
-        "name": teamName, 
+        "matchId": matchId, 
         "goalsScoredAll": goalsScoredAll,
         "goalsScoredHomeOrAway": goalsScoredHomeOrAway,
         "goalsConcededAll": goalsConcededAll,
@@ -58,7 +69,8 @@ def get_team_info(homeMatch, teamId, teamName):
         "goalsScoredAllAvg": goalsScoredAllAvg,
         "goalsScoredHomeOrAwayAvg": goalsScoredHomeOrAwayAvg,
         "goalsConcededAllAvg": goalsConcededAllAvg,
-        "goalsConcededHomeOrAwayAvg": goalsConcededHomeOrAwayAvg
+        "goalsConcededHomeOrAwayAvg": goalsConcededHomeOrAwayAvg,
+        "matchGoals": matchTotalGoals
     }
 
 
@@ -72,12 +84,12 @@ response = requests.get(url, headers=headers)
 homeTeam = response.json()['homeTeam']
 awayTeam = response.json()['awayTeam']
 
-result = {}
-result["matchId"] = MATCH_ID
-result["homeTeam"] = get_team_info(True, homeTeam["id"], homeTeam["name"])
-result["awayTeam"] = get_team_info(False, awayTeam["id"], awayTeam["name"])
-result["matchesNumber"] = NUM_MATCHES
-#result["goalsAvgPredicted"] = 0
+results = {}
+results["matchesNumber"] = NUM_MATCHES
+results["homeTeamName"] = homeTeam["name"]
+results["awayTeamName"] = awayTeam["name"]
+results["matchesHomeTeam"] = get_team_results(True, homeTeam["id"])
+results["matchesAwayTeam"] = get_team_results(False, awayTeam["id"])
 
-with open("matchStats.json", "w") as file:
-    json.dump(result, file, indent=4)
+with open("matchStatsNew.json", "w") as file:
+    json.dump(results, file, indent=4)
